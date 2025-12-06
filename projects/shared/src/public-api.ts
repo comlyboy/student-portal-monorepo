c6a16498fc2f4c48a86e08bf61183fbe01e9e48e
Cornelius Okeke <okekecornelius@gmail.com>
2025-12-06 16:31:02 +0100

    add global search microapp

--- projects/shared/src/public-api.ts
+++ projects/shared/src/public-api.ts
@@ -2,6 +2,20 @@
  * Public API Surface of shared
  */
 
+export * from './lib/components/card-layout/card-layout.component';
+export * from './lib/components/icon.component/icon.component';
+export * from './lib/components/notification/notification.component';
+
 export * from './lib/services/auth.service';
 export * from './lib/services/notification.service';
 export * from './lib/services/custom-event.service';
+
+export * from './lib/guards/auth-guard';
+
+export * from './lib/resolvers/app-resolver';
+
+export * from './lib/interceptors/auth-interceptor';
+export * from './lib/interceptors/error-interceptor';
+
+// export * from './lib/utilities';
+// export * from './lib/constants';
