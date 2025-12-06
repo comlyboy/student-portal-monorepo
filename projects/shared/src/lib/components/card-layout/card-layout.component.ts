882ea6ca99c9c51ce6789c56c0cd08091626c526
Cornelius Okeke <okekecornelius@gmail.com>
2025-12-06 14:56:11 +0100

    add components, resolver, guards, interceptors to shared

---
+++ projects/shared/src/lib/components/card-layout/card-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-card-layout.component',
+  imports: [],
+  templateUrl: './card-layout.component.html',
+  styleUrl: './card-layout.component.css',
+})
+export class CardLayoutComponent {
+
+}
