3dbf0560b912bc936bcc4399ce20017cb9c720e4
Cornelius Okeke <okekecornelius@gmail.com>
2025-12-06 14:02:52 +0100

    add shared module, add environment to portal

---
+++ projects/shared/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of shared
+ */
+
+export * from './lib/services/auth.service';
+export * from './lib/services/notification.service';
+export * from './lib/services/custom-event.service';
