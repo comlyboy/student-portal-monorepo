03d3eb84db472b1a13662f4c49b5527d79f0a99c
Cornelius Okeke <okekecornelius@gmail.com>
2026-01-13 14:42:59 +0100

    add native federation setup

--- projects/portal/src/app/app.ts
+++ projects/portal/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { FederatedComponentDirective } from '@shared/lib/directives/federated-component.directive';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+	imports: [RouterOutlet, FederatedComponentDirective],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
