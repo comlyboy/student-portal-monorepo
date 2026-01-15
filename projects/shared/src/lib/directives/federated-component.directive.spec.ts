fdb66f8236fe8ef1daf2637366789220a4c3ab8f
Cornelius Okeke <okekecornelius@gmail.com>
2026-01-15 23:18:38 +0100

    fixes

--- projects/shared/src/lib/directives/federated-component.directive.spec.ts
+++ projects/shared/src/lib/directives/federated-component.directive.spec.ts
@@ -1,8 +1,54 @@
+import { Component } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
 import { FederatedComponentDirective } from './federated-component.directive';
+import { ModuleFederationService } from '../services/module-federation.service';
 
 describe('FederatedComponentDirective', () => {
-  it('should create an instance', () => {
-    const directive = new FederatedComponentDirective();
-    expect(directive).toBeTruthy();
-  });
+
+	@Component({
+		standalone: true,
+		imports: [FederatedComponentDirective],
+		template: `
+      <ng-container
+        libFederatedComponent
+        [remoteEntry]="'remote'"
+        [exposedModule]="'./Module'"
+        [componentName]="'Cmp'">
+      </ng-container>
+    `
+	})
+	class DummyHostComponent { }
+
+	@Component({
+		standalone: true,
+		template: `<p>Fake Federated Component</p>`
+	})
+	class FakeFederatedComponent { }
+
+	const mfServiceMock = {
+		load: vi.fn().mockResolvedValue({
+			Cmp: FakeFederatedComponent
+		})
+	};
+
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			imports: [DummyHostComponent],
+			providers: [
+				{
+					provide: ModuleFederationService,
+					useValue: mfServiceMock
+				}
+			]
+		});
+	});
+
+	it('should render federated component', async () => {
+		const fixture = TestBed.createComponent(DummyHostComponent);
+		fixture.detectChanges();
+		await fixture.whenStable();
+
+		const html = fixture.nativeElement.innerHTML;
+		expect(html).toContain('Fake Federated Component');
+	});
 });
