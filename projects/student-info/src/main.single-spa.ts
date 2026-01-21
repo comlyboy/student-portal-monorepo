347c2f1b11b94014944ba0911defdd2a985f86a3
Cornelius Okeke <okekecornelius@gmail.com>
2026-01-21 18:05:21 +0100

    feat: refactor application to use single-spa architecture
    - Updated package.json to add single-spa and related dependencies.
    - Removed native federation configuration files for portal and student-info projects.
    - Refactored bootstrap and main files to utilize single-spa for application lifecycle management.
    - Added SystemJS and single-spa scripts to index.html for dynamic loading.
    - Introduced new main.single-spa.ts for single-spa integration in student-info project.
    - Created asset-url.ts and single-spa-props.ts for handling asset paths and custom props in single-spa.
    - Updated styles.scss to include Tailwind CSS directives.
    - Added extra-webpack.config.js for custom webpack configuration.

---
+++ projects/student-info/src/main.single-spa.ts
@@ -0,0 +1,28 @@
+import { Router, NavigationStart } from '@angular/router';
+
+import { bootstrapApplication } from '@angular/platform-browser';
+import { singleSpaAngular, getSingleSpaExtraProviders } from 'single-spa-angular';
+
+import { App } from './app/app';
+import { appConfig } from './app/app.config';
+import { singleSpaPropsSubject } from '../../../single-spa/single-spa-props';
+
+
+const lifecycles = singleSpaAngular({
+	Router,
+	NgZone: 'noop',
+	NavigationStart,
+	template: '<app-root />',
+	bootstrapFunction: (appProps) => {
+		singleSpaPropsSubject.next(appProps);
+		return bootstrapApplication(App, {
+			...appConfig,
+			providers: [...getSingleSpaExtraProviders(), ...appConfig?.providers],
+		})
+	}
+});
+
+
+export const bootstrap = lifecycles.bootstrap;
+export const mount = lifecycles.mount;
+export const unmount = lifecycles.unmount;
--- projects/student-info/src/main.ts
+++ projects/student-info/src/main.ts
@@ -1,6 +1,7 @@
-import { initFederation } from '@angular-architects/native-federation';
+import { bootstrapApplication } from '@angular/platform-browser';
 
-initFederation()
-  .catch(err => console.error(err))
-  .then(_ => import('./bootstrap'))
-  .catch(err => console.error(err));
+import { App } from './app/app';
+import { appConfig } from './app/app.config';
+
+bootstrapApplication(App, appConfig)
+	.catch((err) => console.error(err));
